d028e21573a20f1aa7d7207e753abbc76d31f888
Henok Ashine <henokashine@Henoks-MacBook-Pro.local>
2021-07-15 13:35:42 -0400

    this is my first commit

---
+++ ToDoApp.js
@@ -0,0 +1,13 @@
+
+
+// Selectors
+
+const todoInpout = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+// EventListner
+
+
+// Function
